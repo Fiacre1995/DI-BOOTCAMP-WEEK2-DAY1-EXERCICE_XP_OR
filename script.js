d8d1245e4225d9147bcec18e0fdca614ec980aa6
fiacre <armeldion95@gmail.com>
2022-12-28 08:42:55 +0000

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+/*Exercice 1 */
+let sentence = ["my","favorite","color","is","blue"];
+rejoindre =sentence.toString();
+console.log(rejoindre);
+
+/*Exercice2 */
+let str1 = "mix";
+let str2 = "pod";
+let c = str1 + str2;
+console.log(c);
+
+/*Exercice2 */
+let num1 = prompt('Entrer un Numero');
+alert(`Vous avez entrer ${num1} `); 
+typeof(num1);
+num1 =parseInt(num1);
+console.log(num1)
+let num2 = prompt('Entrer un Numero');
+alert(`Vous avez entrer ${num2} `); 
+num2 =parseInt(num2);
+Somme = num1+num2;
+alert("la somme de nombre 1 et é est : " + Somme );
